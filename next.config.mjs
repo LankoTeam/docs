32f1d99f1b5a3df4d41ec4286f03eae137b33c24
Hikaru <hikaru@nexaorion.tech>
2025-09-07 19:55:39 +0800

    fix:Wrong import

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,15 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: 'dists',
   reactStrictMode: true,
+};
+
+const nextConfig = {
+  output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(config);
