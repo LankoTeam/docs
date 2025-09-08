d35a8fbfbc244883aecdece8c9f7cd833db2fd8f
Hikaru <hikaru@nexaorion.tech>
2025-09-08 20:40:27 +0800

    feat:更新Menu样式&链接

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,23 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import {
+  BookMarked,
+  Pencil,
+  Server,
+} from 'lucide-react';
+import Image from 'next/image';
+
+export const logo = (
+  <>
+    <Image
+      alt="LANKO"
+      src="/lanko-icon.png"
+      sizes="100px"
+      className="hidden w-20 md:w-24 [.uwu_&]:block"
+      aria-label="LANKO"
+      quality={100}
+    />
+  </>
+);
 
 /**
  * Shared layout configurations
@@ -27,12 +46,42 @@ export const baseOptions: BaseLayoutProps = {
   // see https://fumadocs.dev/docs/ui/navigation/links
   links: [
     {
-      text: '快速上手',
-      url: '/getting-start',
+      text: '首页',
+      url: '/'
     },
     {
-      text: 'API',
-      url: '/api/',
+      type: 'menu',
+      text: '文档',
+      url: '/docs/getting-started',
+      items: [
+        {
+          icon: <BookMarked />,
+          text: '使用文档',
+          description: '了解如何使用 LANKO 平台',
+          url: '/docs/usage',
+          menu: {
+            className: 'lg:-span-2',
+          },
+        },
+        {
+          icon: <Server />,
+          text: 'API服务',
+          description: '了解如何使用 LANKO API 服务',
+          url: '/docs/api',          
+          menu: {
+            className: 'lg:col-start-2',
+          },
+        },
+        {
+          icon: <Pencil />,
+          text: '贡献文档',
+          description: '了解 LANKO 的文档撰写规范',
+          url: '/docs/document',
+          menu: {
+            className: 'lg:col-start-3 lg:row-start-1',
+          },
+        }
+      ],
     }
   ],
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,37 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import type { Metadata } from 'next'
+import Head from 'next/head';
+const baseUrl = 'https://docs.lanko.space';
+const metaDescription = `LANKO 是一个更适合中国人的一站式活动服务平台。支持API接入平台，助力开发者轻松构建自己的BOT。无论是线上活动还是线下聚会，LANKO都能为你提供便捷的活动管理和参与体验。`;
+const metaTitle = 'LANKO 文档 - 一站式活动服务平台'
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
+export const metadata: Metadata = {
+  title: { default: metaTitle, template: '%s - LANKO 文档' },
+  description: metaDescription,
+  keywords: `LANKO, Next.js, React, TypeScript, Python, 活动管理, 活动组织, 活动平台, 社交平台, 交友, 聚会, 线上活动, 线下活动, API, 开发者, BOT`,
+  creator: '@LANKO Team',
+  openGraph: {
+    url: baseUrl,
+    title: metaTitle,
+    description: metaDescription,
+    siteName: 'LANKO 文档',
+    type: 'website',
+  },
+}
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <Head>
+        <title>LANKO</title>
+        <link rel="icon" href="/LANKO.svg" />
+      </Head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
